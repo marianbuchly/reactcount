f7c69163c40328508283ef347763c16e12b3f677
marianbuchly <marianbuchly@gmail.com>
2016-05-11 13:35:34 +0200

    add player and store them

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -1,31 +1,35 @@
 import React from 'react';
+import jQuery from 'jquery';
 import Player from './Player';
 import AddPlayer from './AddPlayer';
 
 class ScoreBoard extends React.Component {
-  constructor(){
-      super();
 
-      this.state = {
-          message: "There are no scores yet.",
-          players: [
-                { name: "Marian", score: 0},
-                { name: "Nora", score: 0},
-                { name: "Aggie", score: 0},
-                { name: "Rogier", score: 0},
-                { name: "Laurence", score: 0},
-                { name: "Brian", score: 0},
-                { name: "Benjamin", score: 0},
-                { name: "Wouter", score: 0 },
-                { name: "Rory", score: 0 }
-            ]
-      };
-  }
+    constructor(){
+        super();
 
-  onChangeScore(name, score){
-      var oldPlayers = this.state.players;
-      var newPlayers = oldPlayers.map(function(player){
-          if(player.name == name){
+        this.state = {
+            message: "There are no scores yet.",
+            players: []
+        };
+    }
+
+    componentDidMount(){
+        // the jQuery.get callback will create a new context (this), so we need to remember what 'this'
+        var self = this;
+        jQuery.get("http://nextminds.github.io/scoreboard.json", function(data){
+            self.setState({
+                players: data.players
+            });
+        });
+    }
+
+    onChangeScore(name, score){
+        // create a new list of player by looping over the existing list
+        // and replacing the player we want to change the score for
+        var oldPlayers = this.state.players;
+        var newPlayers = oldPlayers.map(function(player){
+            if(player.name == name){
                 return {
                     name: player.name,
                     score: score
@@ -35,50 +39,64 @@ class ScoreBoard extends React.Component {
             return player;
         });
 
-      this.setState({
-          message: name + " scored and has " + score + " points.",
-          players: newPlayers
+        this.saveData(newPlayers);
+
+        this.setState({
+            message: name + " scored and has " + score + " points.",
+            players: newPlayers
+        });
+    }
+
+    renderPlayer(player){
+        return <Player
+            name={player.name}
+            score={player.score}
+            onChange={this.onChangeScore.bind(this)} />;
+    }
+
+    onAddPlayer(username){
+        var newPlayer = { name: username, score: 0 };
+        var newPlayers = this.state.players.concat(newPlayer);
+        this.setState({
+            players: newPlayers
+        });
+    }
+
+    saveData(newPlayers){
+      jQuery.ajax({
+          type: "POST",
+          url: "http://hook.io/nextminds/scores" ,
+          data: JSON.stringify({
+            players: newPlayers
+
+          }),
+          contentType: "application/json",
+          dataType: "json"
       });
-  }
-
-  renderPlayer(player){
-         return <Player
-             name={player.name}
-             score={player.score}
-             onChange={this.onChangeScore.bind(this)} />;
-     }
-
-  onAddPlayer(username){
-         var newPlayer = { name: username, score: 0 };
-         var newPlayers = this.state.players.concat(newPlayer);
-         this.setState({
-             players: newPlayers
-         });
-     }
+    }
 
     render() {
         return (
-          <div>
-            <table>
-                <thead>
-                  <tr>
-                    <td>Name</td>
-                    <td>Score</td>
-                    <td>+1</td>
-                  </tr>
-                </thead>
-                  <tbody>
-                    {this.state.players.map(this.renderPlayer.bind(this))}
-                  </tbody>
-                <tfoot>
-                  <tr colSpan="3">
-                  <td>{this.state.message}</td>
-                  </tr>
-                </tfoot>
-          </table>
-            <AddPlayer onSubmit={this.onAddPlayer.bind(this)} />
-        </div>
-
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Name</td>
+                            <td>Score</td>
+                            <td>+1</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.players.map(this.renderPlayer.bind(this))}
+                    </tbody>
+                    <tfoot>
+                        <tr colSpan="3">
+                            <td>{this.state.message}</td>
+                        </tr>
+                    </tfoot>
+                </table>
+                <AddPlayer onSubmit={this.onAddPlayer.bind(this)} />
+            </div>
         );
     }
 }
