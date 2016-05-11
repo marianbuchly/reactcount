4e89964e4d89be9eab4056f49f8a63fdc61aeeee
marianbuchly <marianbuchly@gmail.com>
2016-05-11 12:15:27 +0200

    setup counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Counter from './Counter';
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello World!</h1>
+          <div>
+              <h1>Counter</h1>
+              <Counter />
+          </div>
         );
     }
 }
